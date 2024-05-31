2be8d58f059230bd2eff1334eb1a2b655cbd4a06
Ankita Laxmi <74234178+Ankita-tech-26@users.noreply.github.com>
2024-05-31 11:44:19 +0530

    Added

---
+++ useforms/src/App.jsx
@@ -0,0 +1,16 @@
+// src/App.js
+import React from 'react';
+import MyForm from './MyForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Hook Form</h1>
+      <MyForm />
+    </div>
+  );
+}
+
+export default App;
+
